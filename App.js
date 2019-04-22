f9d8e9494f53a0bda075499306c553ce3ab78986
fhc <fhc@ihotel.cn>
2019-04-22 17:42:21 +0800

    加入导航

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from "react";
+import {Button, View, Text} from "react-native";
+import {createStackNavigator, createAppContainer} from "react-navigation";
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
           onPress={() => this.props.navigation.navigate('Details')}
         />
       </View>
     );
   }
 }
+
+const AppNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Details: DetailsScreen,
+});
+export default createAppContainer(AppNavigator);
