1f29175190ae495b62177bcb9500294e27efc07a
fhc <fhc@ihotel.cn>
2019-04-22 18:16:57 +0800

    update

--- App.js
+++ App.js
@@ -1,37 +1,30 @@
-import React from "react";
-import {Button, View, Text} from "react-native";
-import {createStackNavigator, createAppContainer} from "react-navigation";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.push('Details')}
-        />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
+class SettingsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
       </View>
     );
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  Details: DetailsScreen,
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
 });
-export default createAppContainer(AppNavigator);
+
+export default createAppContainer(TabNavigator);
