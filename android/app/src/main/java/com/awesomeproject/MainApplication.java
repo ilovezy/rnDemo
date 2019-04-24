e7df8921beda088f7398aae1f54959f998a205fb
fhc <fhc@ihotel.cn>
2019-04-24 11:00:09 +0800

    test img cropper

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PickerPackage(),
             new SplashScreenReactPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage(),
